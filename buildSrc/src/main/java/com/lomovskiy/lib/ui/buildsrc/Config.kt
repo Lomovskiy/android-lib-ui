0d4714ca4b36a612b3aa2251d18dcf93b14545c2
Alexander Lomovskiy <a@lomovskiy.com>
2021-02-20 18:37:52 +0300

    1.0.6

--- buildSrc/src/main/java/com/lomovskiy/lib/ui/buildsrc/Config.kt
+++ buildSrc/src/main/java/com/lomovskiy/lib/ui/buildsrc/Config.kt
@@ -16,8 +16,8 @@ object Config {
 
     object Versions {
 
-        const val code = 6
-        const val name = "1.0.5"
+        const val code = 7
+        const val name = "1.0.6"
 
         object Android {
 
