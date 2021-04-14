f4c5c70cd55384ceb9a44b9ea6cec429f70d30f5
Alexander Lomovskiy <a@lomovskiy.com>
2021-04-14 23:55:15 +0300

    1.0.9-10

--- buildSrc/src/main/java/com/lomovskiy/lib/ui/buildsrc/Config.kt
+++ buildSrc/src/main/java/com/lomovskiy/lib/ui/buildsrc/Config.kt
@@ -16,8 +16,8 @@ object Config {
 
     object Versions {
 
-        const val code = 9
-        const val name = "1.0.8"
+        const val code = 10
+        const val name = "1.0.9"
 
         object Android {
 
