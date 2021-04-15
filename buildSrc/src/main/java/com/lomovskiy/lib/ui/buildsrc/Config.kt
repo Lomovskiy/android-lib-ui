80c16fa2b1b64062479769e56621043cedaefe5a
Alexander Lomovskiy <a@lomovskiy.com>
2021-04-15 09:23:52 +0300

    1.0.12-12

--- buildSrc/src/main/java/com/lomovskiy/lib/ui/buildsrc/Config.kt
+++ buildSrc/src/main/java/com/lomovskiy/lib/ui/buildsrc/Config.kt
@@ -16,8 +16,8 @@ object Config {
 
     object Versions {
 
-        const val code = 11
-        const val name = "1.0.10"
+        const val code = 12
+        const val name = "1.0.12"
 
         object Android {
 
