513b7d64c83a7cceadbd061bd67e6255bc0edc39
Alexander Lomovskiy <a@lomovskiy.com>
2021-05-13 18:04:29 +0300

    Deprecated SimpleAdapter

--- lib/src/main/java/com/lomovskiy/lib/ui/SimpleAdapter.kt
+++ lib/src/main/java/com/lomovskiy/lib/ui/SimpleAdapter.kt
@@ -3,6 +3,7 @@ package com.lomovskiy.lib.ui
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.RecyclerView
 
+@Deprecated("Use [androidx.recyclerview.widget.ListAdapter] instead")
 abstract class SimpleAdapter<T, VH : RecyclerView.ViewHolder> : RecyclerView.Adapter<VH>() {
 
     abstract val differ: AsyncListDiffer<T>
